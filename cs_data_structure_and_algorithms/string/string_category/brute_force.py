a5fcb7c93d2556361ce6c39ea84009152be0086c
SebastianWilliams666 <929387146@qq.com>
2018-09-10 15:49:54 +0800

    Brute force string matching algorithm

--- cs_data_structure_and_algorithms/string/string_category/brute_force.py
+++ cs_data_structure_and_algorithms/string/string_category/brute_force.py
@@ -2,3 +2,26 @@
 #-*- coding:utf-8 -*-
 # Author:Sebastian Williams
 
+class BruteForce(object):
+    def __init__(self,arr1,arr2):
+        self.array1 = arr1
+        self.array2 = arr2
+
+    def string_match(self):
+        # 蛮力法字符串匹配
+        m=len(self.array1)
+        n=len(self.array2)
+        for i in range(m - n + 1):
+            index = i  # index指向下一个待比较的字符
+            for j in range(len(self.array2)):
+                if self.array1[index] == self.array2[j]:
+                    index += 1
+                else:
+                    break
+                if index - i == len(self.array2):
+                    return i
+        return -1
+
+if __name__ == "__main__":
+    t = BruteForce("adbcbdc", "dc")
+    print(t.string_match())
