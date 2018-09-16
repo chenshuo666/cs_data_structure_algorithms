885d9b9009b3e098d6f33726dc6dffd4e243365e
SebastianWilliams666 <929387146@qq.com>
2018-09-16 15:55:02 +0800

    span of the stack

--- cs_data_structure_and_algorithms/stack/stack_algorithms/span_stack.py
+++ cs_data_structure_and_algorithms/stack/stack_algorithms/span_stack.py
@@ -1,3 +1,25 @@
 #!/usr/bin/python
 #-*- coding:utf-8 -*-
 # Author:Sebastian Williams
+
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.dirname(__file__)+'/'+'..'))
+from stack_category.simply_array_stack import SimplyArrayStack
+
+class AdjacentNumberStack(object):
+    def __init__(self, stack):
+        self.stack = stack
+
+    def get_span(self):
+        spans = []
+        for i in range(self.stack.length):
+            m = 1
+            j = i-1
+            while j >0 and self.stack.items[j] <= self.stack.items[j+1]:
+                m+=1
+                j-=1
+            spans[i] = m
+        return spans
+
+
