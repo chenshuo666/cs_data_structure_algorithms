083bab04c81e70c3be2ebf1753c824a282e3312e
SebastianWilliams666 <929387146@qq.com>
2018-09-16 16:25:40 +0800

    delete adjacent duplicates

--- cs_data_structure_and_algorithms/stack/stack_algorithms/adjacent_number_stack.py
+++ cs_data_structure_and_algorithms/stack/stack_algorithms/adjacent_number_stack.py
@@ -8,18 +8,44 @@ sys.path.append(os.path.abspath(os.path.dirname(__file__)+'/'+'..'))
 from stack_category.simply_array_stack import SimplyArrayStack
 
 class AdjacentNumberStack(object):
-    def __init__(self, stack):
+    def __init__(self, stack, stack1):
         self.stack = stack
+        self.stack1 = stack1
+         
+    def delete_adjacent_number(self):
+        t=-1
+        i=0
+        while i < len(self.stack.items):
+            if t == -1 or self.stack.items[t] != self.stack.items[i]:
+                t+=1
+                self.stack.items[t] = self.stack.items[i]
+                i+=1
+            else:
+                while i < len(self.stack.items) and self.stack.items[t] == self.stack.items[i]:
+                    i+=1
+                t-=1
+        return t+1
+    
+    def get_new_stack(self):
+        index = self.delete_adjacent_number()
+        for i in range(index):
+            self.stack1.push(self.stack.items[i])
 
-    def get_span(self):
-        spans = []
-        for i in range(self.stack.length):
-            m = 1
-            j = i-1
-            while j >0 and self.stack.items[j] <= self.stack.items[j+1]:
-                m+=1
-                j-=1
-            spans[i] = m
-        return spans
+        return self.stack1
+
+
+if __name__ == "__main__":
+    t1 = SimplyArrayStack(20)
+    t2 = SimplyArrayStack(20)
+    a = [1, 67, 5, 5, 67, 354, 54, 54, 2, 2, 2, 354, 876, 85556, 85556]
+    for i in range(len(a)):
+        t1.push(a[i])
+
+    m = AdjacentNumberStack(t1,t2)
+    m.get_new_stack().travel_print()
+
+
+
+
 
 
